7a78249960917af6b13b0f072097d388fee764ac
Malachi Parks <Malachiparks1999@gmail.com>
2022-09-05 05:20:48 +0000

    Tweaked makefile to spit out names so they do not get added via .gitignore. Added .gitignore for C files. Line 31-35 in p1.c are completed the printName function

--- CPractice/w3resource/C Basic Declarations and Expressions/problem1/p1.c
+++ CPractice/w3resource/C Basic Declarations and Expressions/problem1/p1.c
@@ -13,5 +13,49 @@ Mobile : 99-9999999999
 #include "./p1.h"
 
 int main(void){
-    printf("Hello\n");
+    char name[30], *number[11];
+    int month, day, year;
+    printName(name);
 }
+
+/*
+ * Function: printName
+ * ----------------------------
+ *   Prints the name passed in then returns if the command ran successfully
+ *
+ *   Parameters
+ *   name: inputted name to be printed
+ *
+ *   returns: none
+ */
+void printName(char *username){
+    printf("%s","ENTER YOUR FIRST NAME: ");
+    fgets(username, 29, stdin);
+    printf("%s %s%s", "NAME:",username,"\n");
+}
+
+/*
+ * Function: printDOB
+ * ----------------------------
+ *   Prints the name passed in then returns if the command ran successfully
+ *
+ *   Parameters
+ *   month:   inputted month in two digits
+ *   day:     inputted day in two digits
+ *   year:    inputed year in four digits
+ *
+ *   returns: none
+ */
+void printDOB(int month, int day, int year);
+
+/*
+ * Function: printName
+ * ----------------------------
+ *   Prints the name passed in then returns if the command ran successfully
+ *
+ *   Parameters
+ *   cellNum: inputted cell # to be printed
+ *
+ *   returns: none
+ */
+void printNumber(char *cellNum);
